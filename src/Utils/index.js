774bb4581072b407c441ac4a856771e74a9ab862
collins kipkemoi <collinskipkemoi24@gmail.com>
2022-09-07 15:48:16 +0300

    Auth api

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,3 +1,17 @@
 export const saveToLocalStorage = (data, key) => {
     localStorage.setItem(key, data)
 }
+
+
+export const getItemFromStorage = (key) => {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+export const getAccessToken = () => {
+    const token = getItemFromStorage('trackItAuth');
+    if (!token) {
+        return null;
+    }
+
+    return `Bearer ${token.token}`;
+};
